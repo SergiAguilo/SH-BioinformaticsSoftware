9d723f48444d021667adb85239cec09a4670a8ed
emarti2 <eva.martin@bsc.es>
2023-11-02 17:56:10 +0100

    fixed predict function

--- api/predict.py
+++ api/predict.py
@@ -2,27 +2,29 @@ import random
 import tensorflow as tf
 import tensorflow_text as text
 
-# def print_my_examples(inputs, results):
-#   result_for_printing = [f'score: {results[i][0]:.6f}'
-#                          for i in range(len(inputs))]
-#   print(*result_for_printing, sep='\n')
-#   print()
 
 def predict(readme):
-    # 🚧 generate randomly by now 🚧
-    # prediction = random.choice(["bioinformatics", "non-bioinformatics"])
-    # confidence = random.random()
-    #  🚧  #
 
-    reloaded_model = tf.saved_model.load('./bioinfoRepo_bert')
-
-    reloaded_results = tf.sigmoid(reloaded_model(tf.constant(readme)))
+    reloaded_model = tf.saved_model.load('/home/sergi/Desktop/BSC/Biohackaton/SH/contentSH/pipeline/SH-BioinformaticsSoftware/api/bioinfoRepo_bert')
+    reloaded_results = tf.sigmoid(reloaded_model(tf.constant([readme])))
     print(reloaded_results)
     print('Results from the saved model:')
-    print(reloaded_results[0])
-    if reloaded_results[0] < 0.5:
+    print(reloaded_results[0][0])
+    score = float(reloaded_results[0][0])
+    if score < 0.5:
         prediction = ["bioinformatics"]
     else:
         prediction = ["non-bioinformatics"]
+    return prediction, score
 
-    return prediction, reloaded_results[0]
+
+
+
+
+
+
+
+
+
+
+
